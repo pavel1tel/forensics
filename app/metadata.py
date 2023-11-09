b730820a77851a975cb93054d1f2e1bfe800497f
rostyslav <rostmoguchiy@gmail.com>
2023-11-09 20:18:48 +0200

    Rostyslav: Updated project structure

--- app/metadata.py
+++ app/metadata.py
@@ -1,32 +1,35 @@
-import PIL.Image as pil
-from PIL.ExifTags import TAGS
 import glob
 import os
 import time
 
+import PIL.Image as pil
+from PIL.ExifTags import TAGS
+
 # finding metadata of the image
 
 
-def findMetadata(img_path):
+def find_metadata(img_path: str) -> None:
     print("File name: " + img_path)
     img = pil.open(img_path)
     try:
         info = img._getexif()
-        for (tag, value) in info.items():
+        for tag, value in info.items():
             print(str(TAGS.get(tag, tag)) + " | " + str(value))
     except Exception as e:
-        print('Failed to load metadata', e)
+        print("Failed to load metadata", e)
     ti_c = os.path.getctime(img_path)
     ti_m = os.path.getmtime(img_path)
     c_ti = time.ctime(ti_c)
     m_ti = time.ctime(ti_m)
 
     print(
-        f"The file located at the path {img_path} was created at {c_ti} and was lsst modified at {m_ti}")
+        f"The file located at the path {img_path} was created at {c_ti} "
+        f"and was lsst modified at {m_ti}",
+    )
     print("#" * 20)
     print()
 
 
 pattern = "./exif_samples/*/*"
 for img in glob.glob(pattern):
-    findMetadata(img)
+    find_metadata(img)
